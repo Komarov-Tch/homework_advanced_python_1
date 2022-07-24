b734d2be2daca3c9a99e207563a3055b7968b76a
egor <komarovegr@gmail.com>
2022-07-24 19:36:46 +0300

    homework1

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from datetime import datetime
 
-from application.db import people
-
-from application import salary
+import application.salary
+import application.db.people
 
 if __name__ == '__main__':
-    salary.calculate_salary()
-    people.get_employees()
+    application.salary.calculate_salary()
+    application.db.people.get_employees()
     print(datetime.now())
